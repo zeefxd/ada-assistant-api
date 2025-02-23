385d29564202e84812f516b65bb489f1672cf8e8
Marcin <marcdud3k@gmail.com>
2025-02-23 17:21:18 +0100

    feat: add new requirements

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from api.routes import router
+import uvicorn
 
 app = FastAPI(title="Alexa API")
 
 app.include_router(router)
 
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
