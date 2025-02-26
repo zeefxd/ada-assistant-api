cf041195709f1713af8fe93804f6c2d0ff0a2ee6
Marcin <marcdud3k@gmail.com>
2025-02-26 22:48:07 +0100

    feat: update STT to offline mode

--- api/stt.py
+++ api/stt.py
@@ -1,22 +1,42 @@
 from fastapi import APIRouter, UploadFile, File
-import speech_recognition as sr
+from vosk import Model, KaldiRecognizer
 import tempfile
+import json
+import wave
 
 router = APIRouter()
 
+# Ścieżka do modelu https://alphacephei.com/vosk/models/vosk-model-small-pl-0.22.zip
+MODEL_PATH = "./api/vosk"
+
 @router.post("/transcribe")
-async def transcribe_audio(file: UploadFile = File(...), language: str = "pl-PL"):
+async def transcribe_audio(file: UploadFile = File(default=...), language: str = "pl-PL"):
     try:
+        model = Model(MODEL_PATH)
+        
         with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_audio:
             content = await file.read()
             temp_audio.write(content)
-            temp_audio.seek(0)
+            temp_audio.flush()
             
-            recognizer = sr.Recognizer()
-            with sr.AudioFile(temp_audio.name) as source:
-                audio = recognizer.record(source)
-                text = recognizer.recognize_google(audio, language=language)
+            wf = wave.open(temp_audio.name, "rb")
+            
+            recognizer = KaldiRecognizer(model, wf.getframerate())
+            
+            result = ""
+            while True:
+                data = wf.readframes(4000)
+                if len(data) == 0:
+                    break
+                if recognizer.AcceptWaveform(data):
+                    part_result = json.loads(recognizer.Result())
+                    if "text" in part_result:
+                        result += part_result["text"] + " "
+            
+            final_result = json.loads(recognizer.FinalResult())
+            if "text" in final_result:
+                result += final_result["text"]
                 
-        return {"text": text, "language": language}
+        return {"text": result.strip(), "language": language}
     except Exception as e:
         return {"error": str(e)}
