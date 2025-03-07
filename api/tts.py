fa0e2d0f628c0bb9355a15de80478a9dbaca89a9
Marcin <marcdud3k@gmail.com>
2025-03-07 12:28:26 +0100

    feat: update TTS to offline mode

--- api/tts.py
+++ api/tts.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
-from gtts import gTTS
 import tempfile
+import os
+from pathlib import Path
 
 router = APIRouter()
 
@@ -10,16 +11,48 @@ class TextToSpeech(BaseModel):
     text: str
     language: str = "pl"
 
+# https://github.com/coqui-ai/TTS
+tts_model = None
+models_dir = Path(__file__).parent.parent / "model"
+
+def get_tts_model():
+    global tts_model
+    if tts_model is None:
+        try:
+            os.environ["TTS_HOME"] = str(models_dir)
+            models_dir.mkdir(exist_ok=True)
+            print(f"Ładowanie modelu VITS do {models_dir}...")
+            
+            from TTS.api import TTS
+            tts_model = TTS(
+                model_name="tts_models/pl/mai_female/vits", 
+                progress_bar=False
+            )
+            print("Załadowano model VITS!")
+            
+        except Exception as e:
+            raise HTTPException(
+                status_code=500, 
+                detail=f"Nie można załadować modelu TTS: {str(e)}."
+            )
+    return tts_model
+
 @router.post("/synthesize")
 async def synthesize_speech(request: TextToSpeech):
     try:
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_audio:
-            tts = gTTS(text=request.text, lang=request.language)
-            tts.save(temp_audio.name)
-            return FileResponse(
-                temp_audio.name,
-                media_type="audio/mpeg",
-                headers={"Content-Disposition": "attachment; filename=speech.mp3"}
-            )
+        tts = get_tts_model()
+        
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_audio:
+            output_path = temp_audio.name
+        
+        print("Generowanie mowy...")
+        tts.tts_to_file(text=request.text, file_path=output_path)
+        return FileResponse(
+            output_path,
+            media_type="audio/wav",
+            headers={"Content-Disposition": "attachment; filename=speech.wav"}
+        )
     except Exception as e:
-        return {"error": str(e)}
+        import traceback
+        error_details = str(e) + "\n" + traceback.format_exc()
+        raise HTTPException(status_code=500, detail=f"Błąd: {error_details}")
